df52d534f6f89b42f149b67fb558239a1ab61523
Jorge Jimenez <jrg.jima@gmail.com>
2025-04-20 22:06:09 -0600

    Corrijo ruta para model.pkl en Render

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ import os
 app = FastAPI()
 
 
-model_path = os.path.join(os.path.dirname(__file__), "models", "model.pkl")
+model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
 model = joblib.load(model_path)
 
 class DiabetesInput(BaseModel):
